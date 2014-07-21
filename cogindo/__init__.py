ece9229f35482b7dca6fa99de82046f5d96ef775
Pras Velagapudi <psigen@gmail.com>
2014-07-20 23:51:27 -0400

    Changed config to use env variables when avaialble.

--- cogindo/__init__.py
+++ cogindo/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 from flask.ext.mongoengine import MongoEngine
@@ -8,17 +9,21 @@ app.config.from_pyfile('settings.cfg')
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'  # TODO: secure this
 
 # Configure a database client.
-app.config["MONGODB_SETTINGS"] = {"DB": "app27634037",
-                                  "USERNAME": "P5oG6ydpWz",
-                                  "PASSWORD": "a7Uzfg2myw",
-                                  "HOST": "kahana.mongohq.com", "PORT": 10051}
+app.config["MONGODB_SETTINGS"] = \
+    {"DB": os.environ.get("MONGODB_DATABASE", "cogindo"),
+     "USERNAME": os.environ.get("MONGODB_USER", ""),
+     "PASSWORD": os.environ.get("MONGODB_PASS", ""),
+     "HOST": os.environ.get("MONGODB_HOST", "localhost"),
+     "PORT": int(os.environ.get("MONGODB_PORT", "27017"))}
 
 # Configure Google oauth login.
 app.config['SECURITY_POST_LOGIN'] = '/profile'
 app.config['SECURITY_USER_IDENTITY_ATTRIBUTES'] = ['id']
 app.config['SOCIAL_GOOGLE'] = {
-    'consumer_key': '989743370750-67nsgjpaojfbfdpd5fugrih9r8696buj.apps.googleusercontent.com',
-    'consumer_secret': 's0BOlRGv9203qsXYNtB2rSbL'
+    'consumer_key': os.environ.get("SOCIAL_GOOGLE_KEY",
+        '989743370750-67nsgjpaojfbfdpd5fugrih9r8696buj.apps.googleusercontent.com'),
+    'consumer_secret': os.environ.get("SOCIAL_GOOGLE_SECRET",
+        's0BOlRGv9203qsXYNtB2rSbL')
 }
 
 # Initialize flask extensions.
