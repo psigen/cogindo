979c2437f65b79f849c2b17183558cc39e15b910
Pras Velagapudi <psigen@gmail.com>
2014-07-20 23:40:22 -0400

    Fixed port issue.

--- runserver.py
+++ runserver.py
@@ -1,4 +1,6 @@
 #!./flask/bin/python
-
+import os
 from cogindo import app
-app.run(debug=True, host='0.0.0.0')
+
+port = int(os.environ.get("PORT", 5000))
+app.run(debug=True, host='0.0.0.0', port=port)
